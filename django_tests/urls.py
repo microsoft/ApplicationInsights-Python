751a917f98c8ad4e82e45e9629f74f34656d06e9
JJ Jordan <jjjordan@microsoft.com>
2018-06-13 14:15:09 -0700

    Update Django versions in all_tests, fix compatibility issue for 2.0 line

--- django_tests/urls.py
+++ django_tests/urls.py
@@ -4,11 +4,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'aitest.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.home, name='home'),
     url(r'^logger$', views.logger, name='logger'),
     url(r'^thrower$', views.thrower, name='thrower'),
