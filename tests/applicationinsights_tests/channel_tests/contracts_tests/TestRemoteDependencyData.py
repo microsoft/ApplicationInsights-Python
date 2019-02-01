d229e78515f6a1ca382b6c26f698815e9e7fa999
Sasha Pierson <RedSquirrelious@users.noreply.github.com>
2019-02-01 14:49:03 -0800

    no longer testing for removed async prop

--- tests/applicationinsights_tests/channel_tests/contracts_tests/TestRemoteDependencyData.py
+++ tests/applicationinsights_tests/channel_tests/contracts_tests/TestRemoteDependencyData.py
@@ -16,7 +16,7 @@ class TestRemoteDependencyData(unittest.TestCase):
     def test_construct(self):
         item = RemoteDependencyData()
         self.assertNotEqual(item, None)
-    
+
     def test_ver_property_works_as_expected(self):
         expected = 42
         item = RemoteDependencyData()
@@ -27,7 +27,7 @@ class TestRemoteDependencyData(unittest.TestCase):
         item.ver = expected
         actual = item.ver
         self.assertEqual(expected, actual)
-    
+
     def test_name_property_works_as_expected(self):
         expected = 'Test string'
         item = RemoteDependencyData()
@@ -38,7 +38,7 @@ class TestRemoteDependencyData(unittest.TestCase):
         item.name = expected
         actual = item.name
         self.assertEqual(expected, actual)
-    
+
     def test_kind_property_works_as_expected(self):
         expected = object()
         item = RemoteDependencyData()
@@ -49,7 +49,7 @@ class TestRemoteDependencyData(unittest.TestCase):
         item.kind = expected
         actual = item.kind
         self.assertEqual(expected, actual)
-    
+
     def test_value_property_works_as_expected(self):
         expected = 1.5
         item = RemoteDependencyData()
@@ -60,7 +60,7 @@ class TestRemoteDependencyData(unittest.TestCase):
         item.value = expected
         actual = item.value
         self.assertEqual(expected, actual)
-    
+
     def test_count_property_works_as_expected(self):
         expected = 42
         item = RemoteDependencyData()
@@ -71,7 +71,7 @@ class TestRemoteDependencyData(unittest.TestCase):
         item.count = expected
         actual = item.count
         self.assertEqual(expected, actual)
-    
+
     def test_min_property_works_as_expected(self):
         expected = 1.5
         item = RemoteDependencyData()
@@ -82,7 +82,7 @@ class TestRemoteDependencyData(unittest.TestCase):
         item.min = expected
         actual = item.min
         self.assertEqual(expected, actual)
-    
+
     def test_max_property_works_as_expected(self):
         expected = 1.5
         item = RemoteDependencyData()
@@ -93,7 +93,7 @@ class TestRemoteDependencyData(unittest.TestCase):
         item.max = expected
         actual = item.max
         self.assertEqual(expected, actual)
-    
+
     def test_std_dev_property_works_as_expected(self):
         expected = 1.5
         item = RemoteDependencyData()
@@ -104,7 +104,7 @@ class TestRemoteDependencyData(unittest.TestCase):
         item.std_dev = expected
         actual = item.std_dev
         self.assertEqual(expected, actual)
-    
+
     def test_dependency_kind_property_works_as_expected(self):
         expected = object()
         item = RemoteDependencyData()
@@ -115,7 +115,7 @@ class TestRemoteDependencyData(unittest.TestCase):
         item.dependency_kind = expected
         actual = item.dependency_kind
         self.assertEqual(expected, actual)
-    
+
     def test_success_property_works_as_expected(self):
         expected = True
         item = RemoteDependencyData()
@@ -126,18 +126,7 @@ class TestRemoteDependencyData(unittest.TestCase):
         item.success = expected
         actual = item.success
         self.assertEqual(expected, actual)
-    
-    def test_async_property_works_as_expected(self):
-        expected = True
-        item = RemoteDependencyData()
-        item.async = expected
-        actual = item.async
-        self.assertEqual(expected, actual)
-        expected = False
-        item.async = expected
-        actual = item.async
-        self.assertEqual(expected, actual)
-    
+
     def test_dependency_source_property_works_as_expected(self):
         expected = object()
         item = RemoteDependencyData()
@@ -148,12 +137,12 @@ class TestRemoteDependencyData(unittest.TestCase):
         item.dependency_source = expected
         actual = item.dependency_source
         self.assertEqual(expected, actual)
-    
+
     def test_properties_property_works_as_expected(self):
         item = RemoteDependencyData()
         actual = item.properties
         self.assertNotEqual(actual, None)
-    
+
     def test_serialize_works_as_expected(self):
         item = RemoteDependencyData()
         item.ver = 42
@@ -162,7 +151,6 @@ class TestRemoteDependencyData(unittest.TestCase):
         item.duration = 1.5
         item.dependency_kind = object()
         item.success = True
-        item.async = True
         item.dependency_source = object()
         for key, value in { 'key1': 'test value 1' , 'key2': 'test value 2' }.items():
             item.properties[key] = value
