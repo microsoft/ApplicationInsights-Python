a2f17aa9d7ad72c9ca5fec8b2e95d2d0f535fefc
Leighton Chen <lechen@microsoft.com>
2021-04-22 15:46:34 -0700

    version

--- applicationinsights/channel/TelemetryChannel.py
+++ applicationinsights/channel/TelemetryChannel.py
@@ -12,7 +12,7 @@ if sys.version_info >= (3, 0):
 
 # set up internal context
 internal_context = contracts.Internal()
-internal_context.sdk_version = platform_moniker + ':0.12.0'
+internal_context.sdk_version = platform_moniker + ':0.11.10'
 
 
 class TelemetryChannel(object):
