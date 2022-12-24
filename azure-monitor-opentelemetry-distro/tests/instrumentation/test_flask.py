2502bc996d9d117be4e2a074c5dc94d0e54cb64e
Leighton Chen <lechen@microsoft.com>
2022-12-24 13:40:16 -0800

    format

--- azure-monitor-opentelemetry-distro/tests/instrumentation/test_flask.py
+++ azure-monitor-opentelemetry-distro/tests/instrumentation/test_flask.py
@@ -1,3 +1,9 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
 import unittest
 
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
