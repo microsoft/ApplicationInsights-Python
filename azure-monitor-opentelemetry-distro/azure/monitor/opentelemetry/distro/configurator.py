d12ed18231f2c1b0ee12079709dc06a4fe714362
jerevoss <jerevoss@gmail.com>
2023-02-08 14:59:04 -0800

    Removed old log_diagnostic_error calls from configurator

--- azure-monitor-opentelemetry-distro/azure/monitor/opentelemetry/distro/configurator.py
+++ azure-monitor-opentelemetry-distro/azure/monitor/opentelemetry/distro/configurator.py
@@ -5,23 +5,26 @@
 # --------------------------------------------------------------------------
 
 
+import logging
+
 from azure.monitor.opentelemetry.distro._diagnostics._diagnostic_logging import (
     AzureDiagnosticLogging,
 )
 from opentelemetry.sdk._configuration import _OTelSDKConfigurator
 
+_logger = logging.getLogger(__name__)
+
 
 class AzureMonitorConfigurator(_OTelSDKConfigurator):
     def _configure(self, **kwargs):
         try:
+            AzureDiagnosticLogging.enable(_logger)
             super()._configure(**kwargs)
         except ValueError as e:
-            AzureDiagnosticLogging.log_diagnostic_error(
+            _logger.error(
                 f"The components failed to initialize due to a ValueError: {e}"
             )
             raise e
         except Exception as e:
-            AzureDiagnosticLogging.log_diagnostic_error(
-                f"The components failed to initialize: {e}"
-            )
+            _logger.error(f"The components failed to initialize: {e}")
             raise e
