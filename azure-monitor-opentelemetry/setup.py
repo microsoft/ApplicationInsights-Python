66bd929e211e5ec824691bbaf7f7b796656bd2ab
jerevoss <jerevoss@gmail.com>
2023-07-25 09:49:58 -0700

    ~= for exporter

--- azure-monitor-opentelemetry/setup.py
+++ azure-monitor-opentelemetry/setup.py
@@ -85,7 +85,7 @@ setup(
     python_requires=">=3.7",
     install_requires=[
         "azure-core-tracing-opentelemetry~=1.0.0b10",
-        "azure-monitor-opentelemetry-exporter>=1.0.0b15",
+        "azure-monitor-opentelemetry-exporter~=1.0.0b15",
         "opentelemetry-api~=1.19.0",
         "opentelemetry-sdk~=1.19.0",
         "wrapt >= 1.0.0, < 2.0.0",
