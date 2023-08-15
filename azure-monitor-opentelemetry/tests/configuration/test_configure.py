344b3f7842d9aad39f1c86326931a586f12b79f1
Leighton Chen <lechen@microsoft.com>
2023-08-15 11:10:09 -0700

    mock

--- azure-monitor-opentelemetry/tests/configuration/test_configure.py
+++ azure-monitor-opentelemetry/tests/configuration/test_configure.py
@@ -277,9 +277,7 @@ class TestConfigure(unittest.TestCase):
         set_logger_provider_mock.assert_called_once_with(lp_init_mock)
         get_logger_provider_mock.assert_called()
         log_exporter_mock.assert_called_once_with(**configurations)
-        blrp_mock.assert_called_once_with(
-            log_exp_init_mock, schedule_delay_millis=10000
-        )
+        blrp_mock.assert_called_once_with(log_exp_init_mock,)
         lp_init_mock.add_log_record_processor.assert_called_once_with(
             blrp_init_mock
         )
