c02e8da27a19f6fef1566158e582976df6bef73d
Leighton Chen <lechen@microsoft.com>
2023-09-18 13:26:50 -0700

    Update _events.py

--- azure-monitor-events-extension/azure/monitor/events/extension/_events.py
+++ azure-monitor-events-extension/azure/monitor/events/extension/_events.py
@@ -1,6 +1,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-from logging import LogRecord, getLogger, INFO
+from logging import INFO, LogRecord, getLogger
 from typing import Dict, Optional
 
 from opentelemetry.sdk._logs import LoggingHandler
